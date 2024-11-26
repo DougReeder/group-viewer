6eb875983ce09874e2992bd6d3a51dd10a88afad
P. Douglas Reeder <reeder.29@gmail.com>
2024-11-26 10:12:13 -0500

    A, B, X & Y buttons used to change proportions of presentation

--- src/group-viewer.js
+++ src/group-viewer.js
@@ -4,6 +4,7 @@
 
 /* global AFRAME, THREE */
 
+const SCALING_FACTOR = 1.1;
 const MIN_BOUNDING_BOX = new THREE.Vector3(0.000001, 0.000001, 0.000001);
 
 AFRAME.registerComponent('group-viewer', {
@@ -17,12 +18,17 @@ AFRAME.registerComponent('group-viewer', {
 		log: {default: false}
 	},
 
-	events: {
-
-	},
-
 	/** Called once when component is attached. Generally for initial setup. */
 	init: function () {
+		this.handlers.horizontalLarger = this.horizontalLarger.bind(this);
+		this.handlers.horizontalSmaller = this.horizontalSmaller.bind(this);
+		this.handlers.verticalLarger = this.verticalLarger.bind(this);
+		this.handlers.verticalSmaller = this.verticalSmaller.bind(this);
+
+		this.scaleH = 1;
+		this.scaleV = 1;
+		this.offset = new THREE.Vector3();
+
 		const data = this.data;
 		const el = this.el;
 
@@ -50,10 +56,58 @@ AFRAME.registerComponent('group-viewer', {
 		el.sceneEl.appendChild(rig);
 	},
 
+	handlers: {
+	},
+
+	horizontalLarger: function (_evt) {
+		const data = this.data;
+
+		this.scaleH *= SCALING_FACTOR;
+		this.offset.x *= SCALING_FACTOR;
+		this.offset.z *= SCALING_FACTOR;
+
+		const presentationObj = document.getElementById(data.presentationId).object3D;
+		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
+		presentationObj.position.copy(this.offset).add(data.frameCenter);
+	},
+
+	horizontalSmaller: function (_evt) {
+		const data = this.data;
+
+		this.scaleH /= SCALING_FACTOR;
+		this.offset.x /= SCALING_FACTOR;
+		this.offset.z /= SCALING_FACTOR;
+
+		const presentationObj = document.getElementById(data.presentationId).object3D;
+		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
+		presentationObj.position.copy(this.offset).add(data.frameCenter);
+	},
+
+	verticalLarger: function (_evt) {
+		const data = this.data;
+
+		this.scaleV *= SCALING_FACTOR;
+		this.offset.y *= SCALING_FACTOR;
+
+		const presentationObj = document.getElementById(data.presentationId).object3D;
+		presentationObj.scale.setY(this.scaleV);
+		presentationObj.position.y = this.offset.y + data.frameCenter.y;
+	},
+
+	verticalSmaller: function (_evt) {
+		const data = this.data;
+
+		this.scaleV /= SCALING_FACTOR;
+		this.offset.y /= SCALING_FACTOR;
+
+		const presentationObj = document.getElementById(data.presentationId).object3D;
+		presentationObj.scale.setY(this.scaleV);
+		presentationObj.position.y = this.offset.y + data.frameCenter.y;
+	},
+
 	/** Called when properties are changed, incl. right after init */
 	update: function () {
 		const data = this.data;
-		const el = this.el;
 
 		const presentation = document.getElementById(data.presentationId);
 		const presentationObj = presentation.object3D;
@@ -67,16 +121,15 @@ AFRAME.registerComponent('group-viewer', {
 		boundingBox.getSize(bbSize);
 		this.scaleH = this.scaleV = Math.min(data.frameSize.x / bbSize.x, data.frameSize.y / bbSize.y, data.frameSize.z / bbSize.z );
 
-		const offset = new THREE.Vector3();
-		boundingBox.getCenter(offset);
-		offset.multiplyScalar(-this.scaleH);
+		boundingBox.getCenter(this.offset);
+		this.offset.multiplyScalar(-this.scaleH);
 
 		if (data.log) {
-			console.log(`scaleH: ${this.scaleH} scaleV: ${this.scaleV}, offset: ${offset.x} ${offset.y} ${offset.z}`);
+			console.log(`scaleH: ${this.scaleH} scaleV: ${this.scaleV}, offset: ${this.offset.x} ${this.offset.y} ${this.offset.z}`);
 		}
 
 		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
-		presentationObj.position.copy(offset).add(data.frameCenter);
+		presentationObj.position.copy(this.offset).add(data.frameCenter);
 
 		boundingBox.setFromObject(presentationObj);
 		if (data.log) {
@@ -89,9 +142,33 @@ AFRAME.registerComponent('group-viewer', {
 	},
 
 	pause: function () {
+		const data = this.data;
+		if (data.log) {
+			console.log(`pause`)
+		}
+
+		const rightController = document.getElementById('rightController');
+		rightController.removeEventListener('abuttondown', this.handlers.horizontalLarger)
+		rightController.removeEventListener('bbuttondown', this.handlers.horizontalSmaller);
+
+		const leftController = document.getElementById('leftController');
+		leftController.removeEventListener('xbuttondown', this.handlers.verticalLarger);
+		leftController.removeEventListener('ybuttondown', this.handlers.verticalSmaller);
 	},
 
 	play: function () {
+		const data = this.data;
+		if (data.log) {
+			console.log(`play`, this.handlers);
+		}
+
+		const rightController = document.getElementById('rightController');
+		rightController.addEventListener('abuttondown', this.handlers.horizontalLarger);
+		rightController.addEventListener('bbuttondown', this.handlers.horizontalSmaller);
+
+		const leftController = document.getElementById('leftController');
+		leftController.addEventListener('xbuttondown', this.handlers.verticalLarger);
+		leftController.addEventListener('ybuttondown', this.handlers.verticalSmaller);
 	},
 
 	/** Called when a component is removed (e.g., via removeAttribute). */
