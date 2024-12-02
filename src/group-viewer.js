4a596dba91d4290448053c918f6f32df6aacb604
P. Douglas Reeder <reeder.29@gmail.com>
2024-12-02 11:11:12 -0500

    Adds multiuser via aframe-croquet-component

--- src/group-viewer.js
+++ src/group-viewer.js
@@ -6,6 +6,7 @@
 
 const SCALING_FACTOR = 1.1;
 const MIN_BOUNDING_BOX = new THREE.Vector3(0.000001, 0.000001, 0.000001);
+const CROQUET_DELAY = 17;
 
 AFRAME.registerComponent('group-viewer', {
 	dependencies: [],
@@ -16,7 +17,7 @@ AFRAME.registerComponent('group-viewer', {
 		frameSize: {default: {x: 2, y: 2, z: 2}},
 		frameCenter: {default: {x: 0, y: 1, z: 0}},
 
-		log: {default: false}
+		log: {default: false},
 	},
 
 	/** Called once when component is attached. Generally for initial setup. */
@@ -25,10 +26,7 @@ AFRAME.registerComponent('group-viewer', {
 		this.handlers.horizontalSmaller = this.horizontalSmaller.bind(this);
 		this.handlers.verticalLarger = this.verticalLarger.bind(this);
 		this.handlers.verticalSmaller = this.verticalSmaller.bind(this);
-
-		this.scaleH = 1;
-		this.scaleV = 1;
-		this.offset = new THREE.Vector3();
+		this.handlers.keyDown = this.keyDown.bind(this);
 
 		const data = this.data;
 		const el = this.el;
@@ -74,52 +72,122 @@ AFRAME.registerComponent('group-viewer', {
 	horizontalLarger: function (_evt) {
 		const data = this.data;
 
-		this.scaleH *= SCALING_FACTOR;
-		this.offset.x *= SCALING_FACTOR;
-		this.offset.z *= SCALING_FACTOR;
+		const presentation = document.getElementById(data.presentationId);
+		const presentationObj = presentation.object3D;
 
-		const presentationObj = document.getElementById(data.presentationId).object3D;
-		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
-		presentationObj.position.copy(this.offset).add(data.frameCenter);
+		const scale = {x: presentationObj.scale.x * SCALING_FACTOR, y: presentationObj.scale.y, z: presentationObj.scale.z * SCALING_FACTOR}
+
+		const offset = new THREE.Vector3();
+		offset.copy(presentationObj.position).sub(data.frameCenter)
+		offset.x *= SCALING_FACTOR;
+		offset.z *= SCALING_FACTOR;
+		if (data.log) {
+			console.log(`group-viewer horizontalLarger scale: ${JSON.stringify(scale)}  offset: ${JSON.stringify(offset)}`);
+		}
+		offset.add(data.frameCenter);
+
+		presentation.setAttribute('scale', scale);
+		if (! offset.equals(presentationObj.position)) {
+			setTimeout(() => presentation.setAttribute('position', offset), CROQUET_DELAY);
+		}
 	},
 
 	horizontalSmaller: function (_evt) {
 		const data = this.data;
 
-		this.scaleH /= SCALING_FACTOR;
-		this.offset.x /= SCALING_FACTOR;
-		this.offset.z /= SCALING_FACTOR;
+		const presentation = document.getElementById(data.presentationId);
+		const presentationObj = presentation.object3D;
 
-		const presentationObj = document.getElementById(data.presentationId).object3D;
-		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
-		presentationObj.position.copy(this.offset).add(data.frameCenter);
+		const scale = {x: presentationObj.scale.x / SCALING_FACTOR, y: presentationObj.scale.y, z: presentationObj.scale.z / SCALING_FACTOR}
+
+		const offset = new THREE.Vector3();
+		offset.copy(presentationObj.position).sub(data.frameCenter)
+		offset.x /= SCALING_FACTOR;
+		offset.z /= SCALING_FACTOR;
+		if (data.log) {
+			console.log(`group-viewer horizontalSmaller scale: ${JSON.stringify(scale)}  offset: ${JSON.stringify(offset)}`);
+		}
+		offset.add(data.frameCenter);
+
+		presentation.setAttribute('scale', scale);
+		if (! offset.equals(presentationObj.position)) {
+			setTimeout(() => presentation.setAttribute('position', offset), CROQUET_DELAY);
+		}
 	},
 
 	verticalLarger: function (_evt) {
 		const data = this.data;
 
-		this.scaleV *= SCALING_FACTOR;
-		this.offset.y *= SCALING_FACTOR;
+		console.log(`presentation:`, document.querySelectorAll('#' + data.presentationId));
 
-		const presentationObj = document.getElementById(data.presentationId).object3D;
-		presentationObj.scale.setY(this.scaleV);
-		presentationObj.position.y = this.offset.y + data.frameCenter.y;
+		const presentation = document.getElementById(data.presentationId);
+		const presentationObj = presentation.object3D;
+
+		const scale = {x: presentationObj.scale.x, y: presentationObj.scale.y * SCALING_FACTOR, z: presentationObj.scale.z}
+
+		const offset = new THREE.Vector3();
+		offset.copy(presentationObj.position).sub(data.frameCenter)
+		offset.y *= SCALING_FACTOR;
+		if (data.log) {
+			console.log(`group-viewer verticalLarger scale: ${JSON.stringify(scale)}  offset: ${JSON.stringify(offset)}`);
+		}
+		offset.add(data.frameCenter);
+
+		presentation.setAttribute('scale', scale);
+		if (! offset.equals(presentationObj.position)) {
+			setTimeout(() => presentation.setAttribute('position', offset), CROQUET_DELAY);
+		}
 	},
 
 	verticalSmaller: function (_evt) {
 		const data = this.data;
 
-		this.scaleV /= SCALING_FACTOR;
-		this.offset.y /= SCALING_FACTOR;
+		const presentation = document.getElementById(data.presentationId);
+		const presentationObj = presentation.object3D;
 
-		const presentationObj = document.getElementById(data.presentationId).object3D;
-		presentationObj.scale.setY(this.scaleV);
-		presentationObj.position.y = this.offset.y + data.frameCenter.y;
+		const scale = {x: presentationObj.scale.x, y: presentationObj.scale.y / SCALING_FACTOR, z: presentationObj.scale.z};
+
+		const offset = new THREE.Vector3();
+		offset.copy(presentationObj.position).sub(data.frameCenter)
+		offset.y /= SCALING_FACTOR;
+		if (data.log) {
+			console.log(`group-viewer verticalSmaller scale: ${JSON.stringify(scale)}  offset: ${JSON.stringify(offset)}`);
+		}
+		offset.add(data.frameCenter);
+
+		presentation.setAttribute('scale', scale);
+		if (! offset.equals(presentationObj.position)) {
+			setTimeout(() => presentation.setAttribute('position', offset), CROQUET_DELAY);
+		}
+	},
+
+	keyDown: function (evt) {
+		if (evt.metaKey || document.activeElement !== document.body) {
+			return;
+		}
+		switch (evt.key) {
+			case "[":
+				this.horizontalLarger(evt);
+				break;
+			case "]":
+				this.horizontalSmaller(evt);
+				break;
+			case "{":
+				this.verticalLarger(evt);
+				break;
+			case "}":
+				this.verticalSmaller(evt);
+				break;
+			// default:
+			// 	console.log(`no handling for “${evt.key}”`)
+		}
 	},
 
 	/** Called when properties are changed, incl. right after init */
 	update: function () {
 		const data = this.data;
+
+		console.log(`presentation:`, document.querySelectorAll('#' + data.presentationId));
 
 		const presentation = document.getElementById(data.presentationId);
 		const presentationObj = presentation.object3D;
@@ -131,23 +199,27 @@ AFRAME.registerComponent('group-viewer', {
 
 		const bbSize = new THREE.Vector3();
 		boundingBox.getSize(bbSize);
-		this.scaleH = this.scaleV = Math.min(data.frameSize.x / bbSize.x, data.frameSize.y / bbSize.y, data.frameSize.z / bbSize.z );
+		const scale = Math.min(data.frameSize.x / bbSize.x, data.frameSize.y / bbSize.y, data.frameSize.z / bbSize.z );
 
-		boundingBox.getCenter(this.offset);
-		this.offset.multiplyScalar(-this.scaleH);
+		const offset = new THREE.Vector3();
+		boundingBox.getCenter(offset);
+		offset.multiplyScalar(-scale);
 
 		if (data.log) {
-			console.log(`scaleH: ${this.scaleH} scaleV: ${this.scaleV}, offset: ${this.offset.x} ${this.offset.y} ${this.offset.z}`);
+			console.log(`group-viewer update scale: ${JSON.stringify(scale)}  offset: ${JSON.stringify(offset)}`);
 		}
 
-		presentationObj.scale.set(this.scaleH, this.scaleV, this.scaleH);
-		presentationObj.position.copy(this.offset).add(data.frameCenter);
-
-		boundingBox.setFromObject(presentationObj);
-		if (data.log) {
-			const helper = new THREE.Box3Helper( boundingBox, 0xffff00 );
-			document.querySelector('a-scene')?.object3D?.add( helper );
+		presentation.setAttribute('scale', {x: scale, y: scale, z: scale});
+		offset.add(data.frameCenter);
+		if (! offset.equals(presentationObj.position)) {
+			setTimeout(() => presentation.setAttribute('position', offset), CROQUET_DELAY);
 		}
+
+		// if (data.log) {
+		// 	boundingBox.setFromObject(presentation.object3D);
+		// 	const helper = new THREE.Box3Helper( boundingBox, 0xffff00 );
+		// 	document.querySelector('a-scene')?.object3D?.add( helper );
+		// }
 	},
 
 	tick: function (time, timeDelta) {
@@ -156,7 +228,7 @@ AFRAME.registerComponent('group-viewer', {
 	pause: function () {
 		const data = this.data;
 		if (data.log) {
-			console.log(`pause`)
+			console.log(`pause`, this.handlers);
 		}
 
 		const rightController = document.getElementById('rightController');
@@ -166,6 +238,8 @@ AFRAME.registerComponent('group-viewer', {
 		const leftController = document.getElementById('leftController');
 		leftController.removeEventListener('xbuttondown', this.handlers.verticalLarger);
 		leftController.removeEventListener('ybuttondown', this.handlers.verticalSmaller);
+
+		window.removeEventListener('keydown', this.handlers.keyDown);
 	},
 
 	play: function () {
@@ -181,6 +255,8 @@ AFRAME.registerComponent('group-viewer', {
 		const leftController = document.getElementById('leftController');
 		leftController.addEventListener('xbuttondown', this.handlers.verticalLarger);
 		leftController.addEventListener('ybuttondown', this.handlers.verticalSmaller);
+
+		window.addEventListener('keydown', this.handlers.keyDown);
 	},
 
 	/** Called when a component is removed (e.g., via removeAttribute). */
