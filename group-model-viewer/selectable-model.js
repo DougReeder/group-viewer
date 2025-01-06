d8086f9ab4e350a08ff976cbafc1f4556751fe86
P. Douglas Reeder <reeder.29@gmail.com>
2025-01-06 14:46:00 -0500

    Adds inputs to fetch model from URL

--- group-model-viewer/selectable-model.js
+++ group-model-viewer/selectable-model.js
@@ -10,18 +10,40 @@ AFRAME.registerComponent('selectable-model', {
 
 	/** Called once when component is attached. Generally for initial setup. */
 	init: function () {
+		this.handlers.openUrl = this.openUrl.bind(this);
 		this.handlers.openModelFile = this.openModelFile.bind(this);
 		this.handlers.fileInptChange = this.fileInptChange.bind(this);
 		this.handlers.modelLoaded = this.modelLoaded.bind(this);
 		this.handlers.modelError = this.modelError.bind(this);
 
+		const controlStrip = document.createElement('div');
+		controlStrip.style.height = '40px';
+		controlStrip.style.width = '95%';
+		controlStrip.style.position = 'absolute';
+		controlStrip.style.left = '1em';
+		controlStrip.style.bottom = '1em';
+		controlStrip.style.display = 'flex';
+		controlStrip.style.justifyContent = 'flex-start'
+		document.body.appendChild(controlStrip);
+
+		const urlInput = document.createElement('input');
+		urlInput.setAttribute('id', 'urlInput');
+		urlInput.setAttribute('type', 'url');
+		urlInput.setAttribute('placeholder', "Paste a URL to a GLTF or GLB model");
+		urlInput.style.width = '30em';
+		controlStrip.appendChild(urlInput);
+		urlInput.addEventListener('change', this.handlers.openUrl);
+
+		const openUrlBtn = document.createElement('button');
+		openUrlBtn.style.marginLeft = '0.5em';
+		openUrlBtn.innerText = "Fetch model from URL";
+		controlStrip.appendChild(openUrlBtn);
+		openUrlBtn.addEventListener('click', this.handlers.openUrl);
+
 		const openFileBtn = document.createElement('button');
-		openFileBtn.style.height = '40px';
-		openFileBtn.style.position = 'absolute';
-		openFileBtn.style.left = '1em';
-		openFileBtn.style.bottom = '1em';
+		openFileBtn.style.marginLeft = '2em';
 		openFileBtn.innerText = "Select model file";
-		document.body.appendChild(openFileBtn);
+		controlStrip.appendChild(openFileBtn);
 		openFileBtn.addEventListener('click', this.handlers.openModelFile);
 
 		const fileInpt = document.createElement('input');
@@ -38,6 +60,13 @@ AFRAME.registerComponent('selectable-model', {
 
 	handlers: {},
 
+	openUrl: function (evt) {
+		const urlInpt = document.querySelector('input[type=url]');
+		console.log(`openUrl`, evt, `“${urlInpt?.value}”`);
+		this.el.setAttribute('selectable-model', urlInpt.value);
+		this.modelNeedsScaling = true;
+	},
+
 	openModelFile: function (evt) {
 		console.log(`openModelFile`, evt.detail);
 		this.fileInpt.click();
@@ -49,7 +78,7 @@ AFRAME.registerComponent('selectable-model', {
 			if (this.fileInpt.files.length > 0) {
 				const dataUrl = await fileToDataUrl(this.fileInpt.files[0]);
 				if (dataUrl.length > 16384) {
-					this.showPersistentMsg(`“${this.fileInpt.files[0].name}” is too big to sync to other users; upload it somewhere and use the URL`);
+					this.showPersistentMsg(`“${this.fileInpt.files[0].name}” is too big to sync to other users; upload it somewhere and paste the URL below`);
 				}
 				this.el.setAttribute('selectable-model', dataUrl);
 				this.modelNeedsScaling = true;
